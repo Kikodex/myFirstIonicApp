311077ae4a5ba9c7bbb1f3c04474208ad96e9bec
Daniel Castro Rivera <dcastro.rivera.k@gmail.com>
2025-11-09 15:34:31 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'myFirstIonicApp',
+  webDir: 'www'
+};
+
+export default config;
